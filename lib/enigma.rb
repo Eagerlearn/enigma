de88ffead4088f9f1bdea176fb2e12c37bbe5025
craig cardwell <wizeduvcc@gmail.com>
2021-11-11 20:49:46 -0700

    feat: add time converter for date string method and passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,25 @@
 class Enigma
   def initialize
   end
+
+  # def encrypt(encryption, key = nil, date = nil)
+  #
+  #
+  #   require "pry"; binding.pry
+  # end
+
+  #if date is nil use today's date
+  def time_converter_for_date_string
+    date_string_holder = []
+    date_string_holder << Time.now.day.to_s
+    date_string_holder << Time.now.month.to_s
+    date_string_holder << Time.now.year.to_s.delete_prefix("20")
+    date_string_holder.join
+    # date_string_holder.join.to_i * date_string_holder.join.to_i
+  end
+
+  #if key is nil then generate random key
+  # def key_generator_for_encryption
+  #
+  # end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,12 +1,30 @@
 require 'spec_helper'
+require 'date'
 require './lib/enigma'
 
 RSpec.describe Enigma do
   before(:each) do
-    @puzzle = Enigma.new
+    @enigma = Enigma.new
   end
 
   it 'exists' do
-    expect(@puzzle).to be_an_instance_of(Enigma)
+    expect(@enigma).to be_an_instance_of(Enigma)
+  end
+
+  xit '#encrypt' do
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+              }
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it '#time_converter_for_encryption' do
+    expect(@enigma.time_converter_for_date_string).to eq("111121")
+  end
+
+  xit '#key_generator_for_encryption' do
+    expect(@enigma.key_generator_for_encryption).to be(Integer)
   end
 end
