d500838cc9fcb40b0d0dc58dbcf3b84fbf054a81
craig cardwell <wizeduvcc@gmail.com>
2021-11-13 17:51:46 -0700

    feat: add encrypt method to create hash using arguments, still need to complete helper method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,31 +2,45 @@ require 'date'
 
 class Enigma
   def initialize
-    @key_array = []
+    @alphabet_array = ("a".."z").to_a << " "
   end
 
+  #compare message to alphabet_array for shift to do encryption
+  def encryption_helper
+  end
   #if date is nil use today's date helper method
   def time_converter_for_date_string
-    # require "pry"; binding.pry
     date_string_holder = []
     date_string_holder << Time.now.day.to_s
     date_string_holder << Time.now.month.to_s
-    date_string_holder << Time.now.year.to_s.delete_prefix("20")
+    date_string_holder << Time.now.year.to_s[-2..-1]
     date_string_holder.join
-    # date_string_holder.join.to_i * date_string_holder.join.to_i
   end
 
   #if key is nil then generate random key helper method
   def key_generator_for_encryption
-    # require "pry"; binding.pry
     sample_array = (0..9).to_a.sample, (0..9).to_a.sample,
                   (0..9).to_a.sample, (0..9).to_a.sample, (0..9).to_a.sample
     sample_array.join
   end
 
-  # def encrypt(encryption, key = nil, date = nil)
-  #
-  #
-  #   require "pry"; binding.pry
-  # end
+  def encrypt(encryption, key = nil, date = nil)
+    encrypt_hash = Hash.new(0)
+
+    if key == nil && date == nil
+      encrypt_hash = {encryption: encryption, key: key_generator_for_encryption, date: time_converter_for_date_string}
+    elsif key == nil && date != nil
+      encrypt_hash = {encryption: encryption, key: key_generator_for_encryption, date: date}
+    elsif key != nil && date == nil
+      encrypt_hash = {encryption: encryption, key: key, date: time_converter_for_date_string}
+    else
+      encrypt_hash = {encryption: encryption, key: key, date: date}
+
+
+
+    # key_generator_for_encryption
+    # time_converter_for_encryption
+    # encrypt_hash
+    require "pry"; binding.pry
+  end
 end
