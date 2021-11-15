e589d0455ccd529c47d5c749198df31fa5d36759
craig cardwell <wizeduvcc@gmail.com>
2021-11-15 16:23:01 -0700

    test: add decrypt test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -46,5 +46,14 @@ RSpec.describe Enigma do
     }
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
   end
+
+  it '#decrypt' do
+    expected = {
+      encryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+  end
 end
 
