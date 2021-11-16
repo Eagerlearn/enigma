6fe9bbc4b79f772e09d5ee186ac326f7744bb338
craig cardwell <wizeduvcc@gmail.com>
2021-11-15 23:01:28 -0700

    feat: edit encrypt method and work in pry in attempt to get runner file working

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,23 +1,18 @@
 require 'date'
 
 class Enigma
-  # attr_reader :encrypted
-
   def initialize
     @alphabet_array = ("a".."z").to_a << " "
-    #@key = key_generator_for_encryption
-    # @encrypted = Hash.new()
+    # key_string_generator_for_encryption
   end
   # generate keys for encryption offset
   def key_generator_for_encryption
     key_string_array = []
     key_string_to_array = key_string_generator_for_encryption.each_char.to_a
-    # require "pry"; binding.pry
     key_string_to_array.each_cons(2) do |pair|
       key_string_array << pair
     end
     keys_integer_array = key_string_array.map do |key|
-      # require "pry"; binding.pry
       key.join.to_i
     end
     keys_integer_array
@@ -25,12 +20,17 @@ class Enigma
   #if key is nil then generate string helper method
   def key_string_generator_for_encryption
     sample_key = rand(99999).to_s.rjust(5,"0")
+    sample_key
   end
-  #converts the incoming key, if given into an array
-  def key_converter(key)
+  #converts the incoming key, if given into an array or creates and converts
+  def key_converter(key=nil)
+    key_string_generator_for_encryption
     string_array = []
-    # require "pry"; binding.pry
-    string_to_array = key.each_char.to_a
+    if key != nil
+      string_to_array = key.each_char.to_a
+    else
+      string_to_array = key_string_generator_for_encryption.each_char.to_a
+    end
     string_to_array.each_cons(2) do |pair|
       string_array << pair
     end
@@ -59,11 +59,13 @@ class Enigma
     encrypt_array = encryption.split(//)
     dates = date_offset_maker(date)
     keys = key_converter(key)
-
+    # no_key = key_converter(key=nil)
+    # require "pry"; binding.pry
     encrypt_array.each_with_index do |let, index|
-
+# require "pry"; binding.pry
       if index % 4 == 0
         first_letter = @alphabet_array.index(let) + (dates[0] + keys[0])
+        # require "pry"; binding.pry
         new_message_array << @alphabet_array.rotate(first_letter)[0]
       elsif index % 4 == 1
         first_letter = @alphabet_array.index(let) + (dates[1] + keys[1])
